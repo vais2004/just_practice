2fa4354d8c69387ad2890978ecd58cabe638193c
vais2004 <vkawale2004@gmail.com>
2026-02-15 22:20:49 +0530

    update

--- appendToFile.js
+++ appendToFile.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 
 try {
-  fs.appendFileSync("");
-  console.log()
-} catch (error){
-  console.log()
+  fs.appendFileSync("log.txt", "New log entry: Data appended at 9:23PM");
+
+  console.log("Data appended successfully");
+} catch (error) {
+  console.log(error);
 }
